5abb2225ac7e98ff7307a88259ac1f82c2da307b
seunghwan11.kim@lgepartner.com-at-509259651798 <seunghwan.kim@freedgrouptech.com>
2025-05-19 10:15:26 +0900

    feat: add sendWebhook

--- lambda/lambda-function.py
+++ lambda/lambda-function.py
@@ -1,18 +1,18 @@
 import boto3
 import os
+import requests
 
 INSTANCE_ID = os.environ['INSTANCE_ID']
 HOSTED_ZONE_ID = os.environ['HOSTED_ZONE_ID']
 RECORD_NAME = os.environ['RECORD_NAME']
+WEBHOOK_URL = os.environ['WEBHOOK_URL']
 
 ec2 = boto3.client('ec2')
 route53 = boto3.client('route53')
 
-
 def get_public_ip(instance_id):
     response = ec2.describe_instances(InstanceIds=[instance_id])
     return response['Reservations'][0]['Instances'][0]['PublicIpAddress']
-
 
 def get_current_record_ip():
     response = route53.list_resource_record_sets(
@@ -25,7 +25,6 @@ def get_current_record_ip():
     if record_sets and record_sets[0]['Name'] == RECORD_NAME and record_sets[0]['Type'] == 'A':
         return record_sets[0]['ResourceRecords'][0]['Value']
     return None
-
 
 def update_dns_record(ip):
     return route53.change_resource_record_sets(
@@ -46,6 +45,11 @@ def update_dns_record(ip):
         }
     )
 
+def send_webhook_notification(message: str):
+    try:
+        requests.post(WEBHOOK_URL, json={"content": message})
+    except Exception as e:
+        print(f"[ERROR] Webhook failed: {str(e)}")
 
 def lambda_handler(event, context):
     current_ip = get_public_ip(INSTANCE_ID)
@@ -59,14 +63,18 @@ def lambda_handler(event, context):
 
     try:
         update_dns_record(current_ip)
-        print(f"[UPDATED] {RECORD_NAME} changed to {current_ip} from {record_ip}")
+        message = f"✅ [UPDATED] `{RECORD_NAME}` IP changed: `{record_ip}` ➜ `{current_ip}`"
+        print(message)
+        send_webhook_notification(message)
         return {
             'status': 'updated',
             'old_ip': record_ip,
             'new_ip': current_ip
         }
     except Exception as e:
-        print(f"[ERROR] Failed to update {RECORD_NAME} to {current_ip}: {str(e)}")
+        message = f"❌ [FAILED] `{RECORD_NAME}` update to `{current_ip}` failed:\n```{str(e)}```"
+        print(message)
+        send_webhook_notification(message)
         return {
             'status': 'error',
             'message': str(e)
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,18 @@ variable "region" {
   default = "ap-northeast-2"
 }
 
-variable "instance_id" {}
-variable "hosted_zone_id" {}
-variable "record_name" {}
+variable "instance_id" {
+  type = string
+}
+
+variable "hosted_zone_id" {
+  type = string
+}
+
+variable "record_name" {
+  type = string
+}
+
+variable "webhook_url" {
+  type = string
+}
